8e6455c3ca807d12ea98afbb52d7a098170394b6
liuhaoeric <liuhaoeric@didichuxing.com>
2020-06-14 16:53:34 +0800

    删除没用的代码commonutil 添加合并map的utils

--- utils/src/main/java/com/ericliu/common/util/collections/MapUtils.java
+++ utils/src/main/java/com/ericliu/common/util/collections/MapUtils.java
@@ -9,6 +9,14 @@ import java.util.Map;
  */
 public class MapUtils {
 
+    /**
+     * 将src和dst相同的key进行合并，支持递归
+     *
+     * @param src      src
+     * @param dst      dst
+     * @param maxDepth 深度
+     * @return
+     */
     public static Map mergeMap(Map src, Map dst, Integer maxDepth) {
         if (maxDepth < 0) {
             return src;
@@ -46,39 +54,5 @@ public class MapUtils {
                 src.put(dstK, dstV);
             }
         }
-    }
-
-    public static void main(String[] args) {
-        Map a = new HashMap();
-        a.put("a", "a");
-        a.put("aint", 1);
-        Map aChildMap = new HashMap();
-        a.put("aMap", aChildMap);
-        Map aaChildMap = new HashMap();
-        aChildMap.put("a-child-3", aaChildMap);
-        aChildMap.put("a-child-1", 1);
-        aChildMap.put("a-child-2", "a-child");
-
-        aaChildMap.put("a-child-child-2", "a-child-child");
-
-
-        Map b = new HashMap();
-        b.put("a", "b");
-        b.put("aint", 2);
-        b.put("bint", 3);
-        Map bChildMap = new HashMap();
-        b.put("aMap", bChildMap);
-        Map bbChildMap = new HashMap();
-        bChildMap.put("a-child-1", 11);
-        bChildMap.put("a-child-2", "a-child-b");
-        bChildMap.put("a-child-3", bbChildMap);
-        bChildMap.put("b-child-4", "b-child-new");
-
-        bbChildMap.put("a-child-child-2", "b-child-child-2");
-        bbChildMap.put("bb-child-child-3", "bb-child-child-new");
-
-        mergeMap(a, b,0);
-        System.out.println(a);
-
     }
 }
